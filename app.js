cc9f24f90cd0f152fd349435223ddd93008ee596
Laneika <laneika.lyons@student.mec.cuny.edu>
2018-05-16 18:24:00 -0400

    2

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var teamRouter = require('./routes/team');
+var chapterInfoRouter = require('./routes/chapterInfo');
+var contactUsRouter = require('./routes/contactUs');
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/team', teamRouter);
+app.use('/chapterInfo', chapterInfoRouter);
+app.use('/contactUs', contactUsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
